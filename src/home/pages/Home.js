aa1a5bd4ccca86f3aa48df90b3eaca50fd14c6ba
JohnJM <drndrndrndrone@gmail.com>
2021-01-10 19:02:41 +0000

    remove old debug code from home component

--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -5,24 +5,9 @@ import {useSideDrawer} from '../../shared/hooks/SideDrawerHook'
 
 
 const Home = () => {
-
-    const [sideState, toggleOpen, displayOnSide] = useSideDrawer(false)
-
-    // let rngbool;
-
-    // Math.random() <= 0.5 ? rngbool = false : rngbool = true;
-
-    //doesnt work
-    // const changeTheSideBar = useCallback(()=>{
-    //     displayOnSide(<h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>);
-    // }, [])
-
     return (
         <div>
             <p>home works</p>
-
-        {/* doesnt work */}
-            {/* <span onClick={changeTheSideBar}>click to open and chage the sidebar</span> */}
         </div>
     )
 
