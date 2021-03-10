e5e772a3e31472c5af98258793786b32058a5845
JohnJM <drndrndrndrone@gmail.com>
2021-03-10 01:54:25 +0000

    add some style for single thread

--- front/src/home/pages/SingleThreadAndPosts.js
+++ front/src/home/pages/SingleThreadAndPosts.js
@@ -4,53 +4,62 @@ import { useQuery } from 'react-query'
 // import Thread from '../../../../server/models/thread';
 import { getOPAndPosts } from '../../service/ThreadService';
 import SinglePost from '../../home/components/SinglePost';
+import BoardsNav from '../components/BoardsNav';
+
 
 import { AppConfig } from '../../App.config';
 
 
-
-
-
-
 const SingleThreadAndPosts = (props) => {
+   const { board, thread } = useParams();
+   const [ isFullThreadImage,  setIsFullThreadImage] = useState(false);
+   
+   //let location = useLocation();
+   let opContent = {};
+   let postsArr = [];
 
+   const { status, data, error, isFetching, refetch } = useQuery(['OPAndPosts', ['thread_id']], () => getOPAndPosts(thread));
 
-    const { board, thread } = useParams();
+   if(status === 'loading'){
+      return 'loading';
+   } else if(status === 'error'){
+      return 'error';
+   } else {
+      opContent = data.data.OP;
+      postsArr = data.data.Posts;
 
-    let location = useLocation();
-    // const { board_title, thread_id } = location.state;
-    let opContent = {};
-    let postsArr = [];
+      console.log(data.data);
 
-    const { status, data, error, isFetching, refetch } = useQuery(['OPAndPosts', ['thread_id']], () => getOPAndPosts(thread));
-
-    if(status === 'loading'){
-        return 'loading';
-     } else if(status === 'error'){
-        return 'error';
-     } else {
-        opContent = data.data.OP;
-        postsArr = data.data.Posts;
-
-        
-        console.log(data.data);
-        return <>
-            <div>
-                <img src={`${AppConfig.apiUrl}/${opContent.image}`} alt={opContent.subject}/>
-                <h1>{opContent.subject}</h1>
-                <p>{opContent.content}</p>
+      return <>
+         <BoardsNav />
+               <div className="block mb-8">
+                  <span className="text-2xl text-primary">{opContent.subject}</span>
+                  <span className="text-2xl"> | {board} | </span>
+                  <span className="text-2xl"> {opContent.Author} </span>
+               </div>
+         <div class={`flex ${isFullThreadImage ? 'flex-col' : 'flex-row'}`}>
+            <div class="mb-8">
+                  <img onClick={() => setIsFullThreadImage(!isFullThreadImage)} class={`${!isFullThreadImage && 'max-w-250'}`} src={`${AppConfig.apiUrl}/${opContent.image}`} alt={opContent.subject}/>
+                  <p>{opContent.content}</p>
             </div>
 
-            
-            
-            {postsArr.length > 0 && postsArr.map(post => <SinglePost content={post}/>) }
-        </>
-
-
-
-     }
+            <div class={`flex flex-col flex-grow ${!isFullThreadImage && 'pl-4'}`} >
+               {postsArr.length > 0 && postsArr.map(post => <div key={post._id} className="mb-4 w-full border-2 border-primary">
+                     <SinglePost key={post._id} content={post}/>
+                  </div>
+               )}
+         </div>
+         </div>
+      </>
+   }
 }
 
 
 
 export default SingleThreadAndPosts;
+
+//todo
+
+
+   //break this into smaller components?
+   //useFullImage could be a hook - reused on <SinglePost /> too? (instead of line 18)
